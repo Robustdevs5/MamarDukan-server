8706c2d16b24b1b291c24914d618711a316f2cff
Zuned Ahmed <63390198+zunead@users.noreply.github.com>
2021-09-26 18:51:04 +0600

    created orders POST API

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,34 +3,74 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Order = require('../../models/orders');
-
+const Product = require('../../models/product');
 
 // Get all order from database
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Get all order'
-    });
+    Order.find()
+    .select('product quantity _id')
+        .exec()
+        .then(docs => {
+            res.status(200).json({
+                count: docs.length,
+                orders: docs.map(doc => {
+                    console.log("docs",doc)
+                    return {
+                        _id: doc._id,
+                        product: doc.product,
+                        quantity: doc.quantity,
+                        multiVendorSeller: {
+                            type: "GET",
+                            url: "http://localhost:3000/" + doc._id
+                        }
+                    };
+                })
+            });
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+    // res.status(200).json({
+    //     message: 'Get all order'
+    // });
 });
 
 
 // Post an order on database
-router.post('/',(req,res,next) => {
-    const order = new Order({
-        _id: mongoose.Types.ObjectId(),
-        quantity: req.body.quantity,
-        product: req.body.productId
-    })
-    order.save()
+router.post('/',(req, res, next) => {
+    Product.findById(req.body.productId)
+        .then(product => {
+            if (!product) {
+                return res.status(404).json({
+                    message: "Product not fount",
+                });
+            }
+            const order = new Order({
+                _id: mongoose.Types.ObjectId(),
+                quantity: req.body.quantity,
+                product: req.body.productId
+            })
+            return order.save()     
+        })
         .then(result => {
-            console.log(result);
-            res.status(201).json(result);
+            res.status(201).json({
+                message: 'successfully added your order',
+                createdOrder: {
+                    _id: result._id,
+                    product: result.product,
+                    quantity: result.quantity
+                },
+                multiVendorSeller: {
+                    type: "GET",
+                    url: "http://localhost:3000/" + result._id
+                }
+            });
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({
-                error: err
-            });
+            res.status(500).json(err)
         });
+    
     // res.status(201).json({
     //     message: 'successfully added your order',
     //     order: order
