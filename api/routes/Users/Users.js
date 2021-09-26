54cdca0bba510e95f42c550348d8e9f3034919cc
Zuned Ahmed <63390198+zunead@users.noreply.github.com>
2021-09-27 02:01:37 +0600

    created user delete api

--- api/routes/Users/Users.js
+++ api/routes/Users/Users.js
@@ -5,27 +5,37 @@ const bcrypt = require('bcrypt');
 
 const User = require('../../../models/users');
 
+
+
+// signup product on database 
+
 router.post('/signup', (req, res, next) => {
 
     User.find({ email: req.body.email }) 
         .exec()
         .then(user => {
+
             if(user.length >= 1) {
                 return res.status(409).json({
                     message: "Mail exists"
                 });
-            } else {
+            } 
+            
+            else {
                 bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    
                     if(err) {
                         return res.status(500).json({
                             error: err
                         });
                     } else {
+
                         const user = new User({
                             _id: new mongoose.Types.ObjectId(),
                             email: req.body.email,
                             password: hash
                         });
+
                         user.save()
                             .then( result => {
                                 console.log(result)
@@ -38,10 +48,30 @@ router.post('/signup', (req, res, next) => {
                                     error: err
                                 });
                             });
+
                     }
                 });
             }
         })
 });
+
+// Delete the products from database 
+router.delete('/:userId', (req, res, next) => {
+    User.remove({ _id: req.params.userId })
+        .exec()
+        .then( result => {
+            res.status(200).json({
+                message: "User deleted"
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+
 
 module.exports = router;
