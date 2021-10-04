69cd0873d9fa5efcf7e09fb93a3482aeabc91b7a
Zuned Ahmed <63390198+zunead@users.noreply.github.com>
2021-10-04 18:31:29 +0600

    added query filtering

--- api/routes/Products/Products.js
+++ api/routes/Products/Products.js
@@ -10,6 +10,8 @@ const productsControllers = require('../../controllers/Products');
 // ******************** All products route Handlers ****************
 
 router.get('/', productsControllers.get_all_products);
+router.get('/category', productsControllers.get_products_by_category); // Category query
+router.get('/brand', productsControllers.get_products_by_brands);  // Brand query
 router.post('/', productsControllers.add_product);
 router.get('/:productId', productsControllers.get_single_products);
 router.patch('/:productId', productsControllers.updatedProducts);
