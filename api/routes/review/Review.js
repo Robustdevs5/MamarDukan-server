5da1d1c3430ffdc4427704b2ec1a3be12155c60e
Zuned Ahmed <63390198+zunead@users.noreply.github.com>
2021-11-04 19:15:12 +0600

    created review and order api

--- api/routes/review/Review.js
+++ api/routes/review/Review.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 
+
+
 const reviewControllers = require('../../controllers/Review')
 router.get('/', reviewControllers.get_All_Review);
 router.get('/:reviewId', reviewControllers.get_single_review);
